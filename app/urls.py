3e749cc992e8b3a335f7b584bb753b16a377a4d6
“Cory <coryurton@gmail.com>
2022-10-07 23:34:40 -0500

    more to do with converters

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,9 @@ from app import views  # rather than   from app.views import hello, show_details
 urlpatterns = {
     path('', views.hello),
     path('job/<int:id>', views.show_details),
+    path('job/<str:id>', views.show_details),
 }
+
+# path converters  slug   uuid    str     int
+# slug matches any slug string, and contains only letters numbers underscores and hyphens   "my-first-blog"
+# universally unique identifier  e.g:  c5ecbde1-cbf4-11e5-a759
