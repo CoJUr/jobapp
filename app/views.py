f9605a0764000c4487a7282aa97db112c74459cf
“Cory <coryurton@gmail.com>
2022-10-07 22:33:09 -0500

    passing variables for dynamic responses, pausing

--- app/views.py
+++ app/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+job_title = [
+    "First Job",
+    "Second Job"
+]
+
+job_description = [
+    "First job description",
+    "Second job description"
+]
 # Create your views here.
 # views (classes) defined here
 
 
 def hello(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("<h3>Hello World</h3>")
 
 
 def show_details(request, id):
-    return HttpResponse(f"Job detail page {id}")
+    # return HttpResponse(f"<h3>Job detail page {id}<h3>")
+    # site = "https://google.com"
+    return_html = f"<h1>{job_title[id]}</h1>  <h3>{job_description[id]}</h3>"
+    return HttpResponse(f"Visit <a href={site}>Google here </a>")
