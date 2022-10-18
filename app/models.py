dc3edbd1656f76dcdde7d99c8121749f6043f422
“Cory <coryurton@gmail.com>
2022-10-18 16:23:31 -0500

    database migrations intro

--- app/models.py
+++ app/models.py
@@ -1,4 +1,15 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
-# operations like CRUDing models here
+# Create your models here.       operations like CRUDing models here
+
+
+class JobPost(models.Model):
+    # fields
+    title = models.CharField(max_length=200)
+
+# commands for migrations:
+# makemigrations -> creates new migrations based on identified changes in models
+# sqlmigrate -> displays the sql statement that would be triggered
+# migrate -> running, applying, unapplying the migrations
+# showmigrations -> lists the migrations and their statuses
