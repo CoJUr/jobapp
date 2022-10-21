da1fd27226b809f798cc5fb04763c04f463e4564
“Cory <coryurton@gmail.com>
2022-10-20 22:47:01 -0500

    __str__ method 101

--- app/models.py
+++ app/models.py
@@ -11,8 +11,17 @@ class JobPost(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     salary = models.IntegerField()
 
+# __str__ method allows defining the string representation of the objects, like .toString
+    def __str__(self):
+        return self.title
+
 # commands for migrations:
 # makemigrations -> creates new migrations based on identified changes in models
 # sqlmigrate -> displays the sql statement that would be triggered
 # migrate -> running, applying, unapplying the migrations
 # showmigrations -> lists the migrations and their statuses
+
+# python manage.py makemigrations <app-name>   - checks in specified app for changes and
+# creates migration in appname/migrations directory
+
+# exit() | python manage.py shell | from app.models import JobPost | JobPost.objects.all()
